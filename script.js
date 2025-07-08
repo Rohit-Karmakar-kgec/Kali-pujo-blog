56ed146ce8c69816e15f255d54de0edbd032f3aa
Rohit-Karmakar-kgec <karmakar.rohit2306@gmail.com>
2025-07-08 10:55:33 +0530

    Solved responsiveness issue of side-navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function showSideNav() {
     const sideBar = document.querySelector('.side-nav-bar')
-    sideBar.style.right = '0%'
-    sideBar.style.left = 'auto'
+    sideBar.style.width = '50%'
     const sideBarSwitch = document.querySelector('.sideBarOpen')
     sideBarSwitch.style.opacity = '0'
     const bg = document.querySelector('.behind-sidenav')
@@ -9,7 +8,7 @@ function showSideNav() {
 }
 function closeSideNav() {
     const sideBar = document.querySelector('.side-nav-bar')
-    sideBar.style.right = '-100%'
+    sideBar.style.width = '0%'
     const sideBarSwitch = document.querySelector('.sideBarOpen')
     sideBarSwitch.style.opacity = '1'
     const bg = document.querySelector('.behind-sidenav')
