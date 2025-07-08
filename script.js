770ceb1ffa2c4bafdfdabafee8760c248405fa3c
Rohit-Karmakar-kgec <karmakar.rohit2306@gmail.com>
2025-07-08 12:40:02 +0530

    Updated UI of side-navbar

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function showSideNav() {
     sideBarSwitch.style.opacity = '0'
     const bg = document.querySelector('.behind-sidenav')
     bg.style.width = '100%'
+
+    const body = document.querySelector('body')
+    body.style.overflow = 'hidden'
 }
 function closeSideNav() {
     const sideBar = document.querySelector('.side-nav-bar')
@@ -13,4 +16,7 @@ function closeSideNav() {
     sideBarSwitch.style.opacity = '1'
     const bg = document.querySelector('.behind-sidenav')
     bg.style.width = '0%'
+
+    const body = document.querySelector('body')
+    body.style.overflow = 'visible'
 }
